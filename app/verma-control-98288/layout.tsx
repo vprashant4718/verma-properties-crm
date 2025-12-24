ce9a44de59a454f59ae5a0d289b09b4d31db2ae1
Prashant Verma <vprashant4718@gmail.com>
2025-12-24 13:56:52 +0530

    updated next version latest

--- app/verma-control-98288/layout.tsx
+++ app/verma-control-98288/layout.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import { SidebarProvider } from "@/components/ui/sidebar"
 import { AppSidebar } from "./_components/app-sidebar"
 import { useEffect, useState } from "react"
@@ -6,28 +7,23 @@ import { useRouter } from "next/navigation"
 import AppHeader from "./_components/AppHeader"
 import "../globals.css"
 
-
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [user] = useState(false)
+  const router = useRouter()
 
-  const [user] = useState(false);
-  const router = useRouter();
+  useEffect(() => {
+    if (!user) {
+      router.replace("/verma-control-98288/Login")
+    }
+  }, [user, router])
 
-    useEffect(()=>{
-      if(!user){
-        router.replace("/verma-control-98288/Login")
-      }
-      else{
-        router.replace("/verma-control-98288")
-      }
-    },[])
   return (
     <SidebarProvider>
       <AppSidebar />
       <main className="w-full px-2">
         <AppHeader />
-        <div className=" p-5 ">
-        {children}
-
+        <div className="p-5">
+          {children}
         </div>
       </main>
     </SidebarProvider>
